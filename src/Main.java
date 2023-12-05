64f7ae3d3a59c344b1899efecf391faf9e9e4a02
The Code Designer <gazdagbal@gmail.com>
2023-12-05 13:32:03 +0100

    Geting in the numbers and parsing them

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("$$$ Lottery Winning Simulator! $$$!");
+        userInput();
     }
+
+    public static void userInput(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter 5 numbers! (They must be between 1-90)");
+        System.out.println("They must be separated with a space!");
+        String input = in.nextLine();
+
+        String[] strings = input.split(" ");
+        System.out.println(Arrays.toString(strings));
+        int[] numbers = new int[5];
+
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = Integer.parseInt(strings[i]);
+        }
+
+    }
+
 }
